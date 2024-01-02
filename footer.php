3e23741dc8a864f03b06b7c718f1309f405da64b
atiikmahmud <atikmahmud.cse@gmail.com>
2024-01-02 16:47:59 +0600

    Theme Updated

--- footer.php
+++ footer.php
@@ -3,33 +3,35 @@
         <div class="row">
             <div class="col-md-6">
                 <?php
-                    if (is_active_sidebar("footer-left")){
-                        dynamic_sidebar("footer-left");
-                    }
+                if(is_active_sidebar("footer-left")){
+                    dynamic_sidebar("footer-left");
+                }
                 ?>
             </div>
             <div class="col-md-6">
-	            <?php
-	            if (is_active_sidebar("footer-right")){
-		            dynamic_sidebar("footer-right");
-	            }
-	            ?>
+                <?php
+                if(is_active_sidebar("footer-right")){
+                    dynamic_sidebar("footer-right");
+                }
+                ?>
 
                 <div class="footermenu">
-	                <?php
-	                wp_nav_menu(
-		                array(
-			                'theme_location'    => 'footermenu',
-			                'menu_id'           => 'footermenucontainer',
-			                'menu_class'        => 'list-inline text-right',
-		                )
-	                )
-	                ?>
+                <?php
+                wp_nav_menu(
+                    array(
+                        'theme_location' => 'footermenu',
+                        'menu_id'        => 'footermenucontainer',
+                        'menu_class'     => 'list-inline text-right',
+                    )
+                );
+                ?>
                 </div>
+
             </div>
         </div>
     </div>
 </div>
+
 <?php wp_footer(); ?>
 </body>
 </html>
