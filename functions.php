1c30f0778c835c8c9e1251b149e211105b03a5cd
atiikmahmud <atikmahmud.cse@gmail.com>
2024-01-03 12:19:05 +0600

    custom header text change by customizer

--- functions.php
+++ functions.php
@@ -12,7 +12,12 @@ function alpha_bootstrapping() {
     load_theme_textdomain( "alpha" );
     add_theme_support( "post-thumbnails" );
     add_theme_support( "title-tag" );
-    add_theme_support( "custom-header" );
+    $alpha_custom_header_details = array(
+        'header-text'           => true,
+        'default-text-color'    => '#222',
+    );
+    add_theme_support( "custom-header", $alpha_custom_header_details);
+    add_theme_support("header-text");
     register_nav_menu( "topmenu", __( "Top Menu", "alpha" ) );
     register_nav_menu( "footermenu", __( "Footer Menu", "alpha" ) );
 }
@@ -120,6 +125,24 @@ function alpha_about_page_template_banner(){
                     background-image: url(<?php echo header_image(); ?>);
                     background-size: cover;
                     margin-bottom: 50px;
+                }
+
+                .header h1.heading a, h3.tagline {
+                    color: #<?php echo get_header_textcolor(); ?>;
+
+                    <?php
+                    if(!display_header_text()){ 
+                        echo "display: none;";
+                    }
+                    ?>
+                }
+
+                .header h1.heading{
+                    <?php
+                    if(!display_header_text()){ 
+                        echo "border: none !important;";
+                    }
+                    ?>
                 }
             </style>
             <?php
