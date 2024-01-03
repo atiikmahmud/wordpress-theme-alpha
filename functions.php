66268b5b3e2e5e4585d2d4324f7b8af51dce6110
atiikmahmud <atikmahmud.cse@gmail.com>
2024-01-03 11:48:02 +0600

    Header image change by customizer

--- functions.php
+++ functions.php
@@ -12,6 +12,7 @@ function alpha_bootstrapping() {
     load_theme_textdomain( "alpha" );
     add_theme_support( "post-thumbnails" );
     add_theme_support( "title-tag" );
+    add_theme_support( "custom-header" );
     register_nav_menu( "topmenu", __( "Top Menu", "alpha" ) );
     register_nav_menu( "footermenu", __( "Footer Menu", "alpha" ) );
 }
@@ -109,6 +110,20 @@ function alpha_about_page_template_banner(){
         }
         </style>
         <?php
+    }
+
+    if(is_front_page()){
+        if(current_theme_supports("custom-header")){
+            ?>
+            <style>
+                .header{
+                    background-image: url(<?php echo header_image(); ?>);
+                    background-size: cover;
+                    margin-bottom: 50px;
+                }
+            </style>
+            <?php
+        }
     }
 }
 add_action("wp_head", "alpha_about_page_template_banner", 11);
